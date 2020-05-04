8c7576e6c63ed431a370dd769bff6e5519fc32ce
Azatbek <kanybekov66@gmak.com>
2020-05-04 18:31:21 +0600

    Updates

---
+++ precache-manifest.1ef46ea14bc219d2520cc27ad5e33186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9afdf7772e9487f7095101ef0b4afa1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "291483c896e5d4404c71",
+    "url": "/robofriends/static/css/2.9c6019a0.chunk.css"
+  },
+  {
+    "revision": "ee2fb39e064053bf479e",
+    "url": "/robofriends/static/css/main.cfdea20c.chunk.css"
+  },
+  {
+    "revision": "291483c896e5d4404c71",
+    "url": "/robofriends/static/js/2.7dfcfc6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7dfcfc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2fb39e064053bf479e",
+    "url": "/robofriends/static/js/main.9fe0c994.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
